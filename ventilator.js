31075258b38832e125a470483b1f15aefec29831
Pranathi1309 <72182937+Pranathi1309@users.noreply.github.com>
2020-10-01 22:44:30 +0530

    Delete ventilator.js

--- ventilator.js
+++
@@ -1,9 +0,0 @@
-var mongoose =require("mongoose");
-var v_schema=mongoose.Schema({
-	hid:String,
-	vid:String,
-	status:String,
-	hname:String
-})
-var vent=mongoose.model("vent",v_schema);
-module.exports=vent;
